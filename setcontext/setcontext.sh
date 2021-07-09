3b9fabe630606e8e32f57cb2a88cb9de40540471
amovfx <amo.vfx@protonmail.com>
2021-07-09 16:40:43 -0400

    Remove context function deleetes gcloud project, conda env, remote repo and local repo.

--- setcontext/setcontext.sh
+++ setcontext/setcontext.sh
@@ -3,15 +3,45 @@ setcontext () {
   eval `python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py setcontext $1`;
 }
 
-delcontext () {
+
+
+removecontext () {
   #this is meant to destr
-  local context=$1;
-	tput setaf 1; echo Removing conda evn;
-	conda remove $1;
-	local project_name=$(echo $1 | sed "s/[^[:alnum:]-]//g");
-	gcloud projects delete $project_name --quiet;
-	rm -rf ~/git/$1;
+  conda activate amo-prj-utilities
+  if [ $# -eq 0 ]
+    then
+      echo "No arguments"
+      local project=$(basename "$PWD")
+    else
+      local project=$1
+  fi
+  echo "Removing context: $project"
+
+  #set directory
+  cd ~/git/
+
+  #delete google-project
+  gcloud projects delete $project
+
+  #remove conda env
+  conda env remove -n $project
+
+  #remove remote repo
+  local git_user=$(git config user.name)
+  local access_token=$(cat /Users/andrewoseen/git/amo-prj-utilities/setcontext/delete_repo_token)
+  echo "Removing https://api.github.com/repos/$git_user/$project"
+  curl -u $git_user:$access_token -X "DELETE" https://api.github.com/repos/$git_user/$project
+
+  #remove folder
+  rm -rf ~/git/"$project"
+
+  #clear environment variables
+  eval `python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py clear_context_env_variables`;
+  eval `python /Users/andrewoseen/git/amo-prj-utilities/setcontext/setcontext.py set_terminal_prompt`
+
 }
+
+
 
 delcontexts () {
   for d in $1; do
@@ -34,17 +64,44 @@ del_conda_envs () {
 
 }
 
+list_repos () {
+  local git_user=$(git config user.name)
+  curl "https://api.github.com/users/$git_user/repos?per_page=100" | grep -o 'git@[^"]*'
+}
+
 del_remote_repo () {
   local git_user=$(git config user.name)
   local access_token=$(cat /Users/andrewoseen/git/amo-prj-utilities/setcontext/delete_repo_token)
   echo "$access_token"
   local git_repos=$(curl "https://api.github.com/users/$git_user/repos?per_page=100" | grep -o 'git@[^"]*')
-  #curl -u amovfx:key -X "DELETE" https://api.github.com/repos/amovfx/test
-  for repo in $git_repos
-  do
-    echo $repo
+  local repo_array=($(python -c "import sys; print(' '.join(sys.argv[1].splitlines()))" $git_repos))
+  for repo in $repo_array; do
+    repo_name=$(echo $repo | awk -F/ '{print $2}' | awk -F. '{print $1}')
+    echo "Delete $repo_name (y/n)?";
+    read CONT </dev/tty;
+      if [ "$CONT" = "y" ]; then
+        echo Deleteing "repo_name"
+        echo "-u $git_user:$access_token -X /"DELETE/" https://api.github.com/repos/$git_user/$repo_name"
+        curl -u $git_user:$access_token -X "DELETE" https://api.github.com/repos/$git_user/$repo_name
+      else
+        echo "Skipping $repo_name"
+      fi
   done
-
 }
+
+del_local_repo () {
+  for repo in ~/git/*/; do
+    echo "Delete $repo? (y/n)?"
+    read CONT </dev/tty;
+    if [ "$CONT" = "y" ]; then
+      echo "deleting $repo"
+      folder_name=$(echo $repo)
+      rm -rf "$repo"
+    else
+      echo "Skipping..."
+    fi
+  done
+}
+
 
 
